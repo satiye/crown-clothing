518b246b336b2b761fd4350d110c57e5d5eb38db
Satiye ÇATAL <satiyecatal@192.168.68.106>
2021-06-05 15:37:01 +0300

    menu-item is updated with withRouter HOF

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
 
+const HatPage = () => (
+  <div>
+      <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/hats' component={HatPage} />
+      </Switch>
     </div>
   );
 }
